8fc06dcd573879b6d0ee58728bd89d0bb53d95fd
Caio Lucas <lcaio1281@gmail.com>
2023-04-02 23:23:00 -0300

    feat: initial Home page component

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {View, Text, StyleSheet, SafeAreaView} from 'react-native';
+
+export function Home() {
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello world!</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#121214',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#121214',
+    paddingHorizontal: 30,
+    paddingVertical: 50,
+  },
+  title: {
+    color: '#f1f1f1',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
