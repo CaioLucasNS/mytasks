a33a8d10fd09805514903fac27b1cac44dcf8391
Caio Lucas <lcaio1281@gmail.com>
2023-04-05 10:55:47 -0300

    feat: add jest coverage report

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   preset: 'react-native',
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   testPathIgnorePatterns: ['/node_modules', '/android', '/ios'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.tsx', '!src/**/*.spec.tsx'],
+  coverageReporters: ['lcov'],
 };
