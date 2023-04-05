c502271cb23a71a53000dc17cc3a5aa21196a9e8
Caio Lucas <lcaio1281@gmail.com>
2023-04-04 23:24:26 -0300

    feat: add Home test - custom hook useTaskList

--- __tests__/pages/home.spec.tsx
+++ __tests__/pages/home.spec.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import {render} from '@testing-library/react-native';
+import {renderHook, act} from '@testing-library/react-hooks';
+
 import {Home} from '../../src/pages/Home';
+import {TasksProvider, useTaskList} from '../../src/context/TasksContext';
 
 describe('Home page', () => {
   it('should render Home page', () => {
@@ -9,5 +12,19 @@ describe('Home page', () => {
     const inputNewTask = getByPlaceholderText('Nova tarefa');
     expect(inputNewTask).toBeDefined();
     expect(inputNewTask.props.placeholder).toBeTruthy();
+  });
+
+  it('should add 1 item on tasks list', async () => {
+    const {result} = renderHook(() => useTaskList(), {
+      wrapper: TasksProvider, // passing provider
+    });
+
+    const data = {id: 'task01', title: 'task01'};
+
+    await act(() => {
+      return result.current.addTask(data);
+    });
+
+    expect(result.current.tasks).toBeTruthy();
   });
 });
